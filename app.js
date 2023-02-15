f2b8108d479c344581af0b3b591acbc1353cdfc7
moinuddin2004 <abdullahsajid1098@gmail.com>
2023-02-15 10:18:58 +0500

    Js practics

---
+++ app.js
@@ -0,0 +1,59 @@
+
+
+
+/**function subtractingNumbers(n1, n2) {
+    return n1 - n2
+    console.log(n1 - n2)
+}
+
+console.log(subtractingNumbers(55 , 45))
+alert(subtractingNumbers(60 ,35))
+document.write(subtractingNumbers(45 , 25)+ "</br>")
+
+
+function addingNumbers(a , b) {
+    return a + b
+}
+
+console.log(addingNumbers(25,75))
+console.log(addingNumbers(2,7))
+alert(addingNumbers(7,15))
+alert(addingNumbers(8,20))
+document.write(addingNumbers(16,26)+ "</br>")
+document.write(addingNumbers(15,65)+ "</br>")**/
+
+//Normal Expression
+
+
+const es6NormalExpression = function(x,y) {
+    console.log(x*y)
+
+}
+
+es6NormalExpression(5,4)
+
+//arrow Function
+
+const es6NormalExpression2 = (x,y) => {
+    console.log(x+y)
+}
+
+es6NormalExpression2(6,8)
+
+
+//3rd Function
+
+
+const es6NormalExpression3 = (x,y) => {
+    return x + y
+    
+}
+
+
+console.log(es6NormalExpression3(2,3))
+
+
+const es6NormalExpression4 = (x,y) => x+y
+
+
+console.log(es6NormalExpression4(20,33))
