cee9007e85a2871909a7d15ee718e825280c6b3c
moinuddin2004 <abdullahsajid1098@gmail.com>
2023-02-17 10:21:45 +0500

    Arrays work

--- app.js
+++ app.js
@@ -1,59 +1,31 @@
+//Arrays
+var students=["javed","hassan","mujtuba","Ashad maha"]
 
+//For Adding in Start
+var checkingTheChange=students.unshift("Khurram")
+console.log(students)
+console.log(checkingTheChange)
 
+//Removing from start
+var checkingChange=students.shift()
+console.log(students)
+console.log(checkingChange)
 
-/**function subtractingNumbers(n1, n2) {
-    return n1 - n2
-    console.log(n1 - n2)
-}
+//Adding in End
+var checkingPush=students.push("tayyab")
+console.log(students)
+console.log(checkingPush)
 
-console.log(subtractingNumbers(55 , 45))
-alert(subtractingNumbers(60 ,35))
-document.write(subtractingNumbers(45 , 25)+ "</br>")
+//Removing From End
 
+var checkingTheResult=students.pop()
+console.log(students)
+console.log(checkingTheResult)
 
-function addingNumbers(a , b) {
-    return a + b
-}
+var splice=students.splice(0,2,"Reyan")
+console.log(students)
+console.log(splice)
 
-console.log(addingNumbers(25,75))
-console.log(addingNumbers(2,7))
-alert(addingNumbers(7,15))
-alert(addingNumbers(8,20))
-document.write(addingNumbers(16,26)+ "</br>")
-document.write(addingNumbers(15,65)+ "</br>")**/
-
-//Normal Expression
-
-
-const es6NormalExpression = function(x,y) {
-    console.log(x*y)
-
-}
-
-es6NormalExpression(5,4)
-
-//arrow Function
-
-const es6NormalExpression2 = (x,y) => {
-    console.log(x+y)
-}
-
-es6NormalExpression2(6,8)
-
-
-//3rd Function
-
-
-const es6NormalExpression3 = (x,y) => {
-    return x + y
-    
-}
-
-
-console.log(es6NormalExpression3(2,3))
-
-
-const es6NormalExpression4 = (x,y) => x+y
-
-
-console.log(es6NormalExpression4(20,33))
+var diffrenciatingName= students.slice(0,1)
+console.log(students)
+console.log(diffrenciatingName)
