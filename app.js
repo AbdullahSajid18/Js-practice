a5c69a130114d1e0539b875372b313c674140af1
moinuddin2004 <abdullahsajid1098@gmail.com>
2023-02-22 09:48:20 +0500

    JS class work

--- app.js
+++ app.js
@@ -1,69 +1,30 @@
-//Arrays
-/**var students=["javed","hassan","mujtuba","Ashad maha"]
+const theDetailsFromTheDatabase = {
+    firstName: "Syed",
+    lastName: "Hamza",
+    mobileNumber: "03190374204",
+    password: 626985,
+    dateOfBirth: 06,
+    monthOfBirth: 02,
+    yearOfBirth: 2004 ,
+    gender: "male"
+}
 
-//For Adding in Start
-var checkingTheChange=students.unshift("Khurram")
-console.log(students)
-console.log(checkingTheChange)
+const mobileNumberOfUser = prompt("Type Your Mobile Number")
+const passwordOfUser = prompt("Type Your password")
 
-//Removing from start
-var checkingChange=students.shift()
-console.log(students)
-console.log(checkingChange)
+const detailsOfUser  = {
+    mobileNumber : mobileNumberOfUser,
+    password: passwordOfUser
+}
 
-//Adding in End
-var checkingPush=students.push("tayyab")
-console.log(students)
-console.log(checkingPush)
-
-//Removing From End
-
-var checkingTheResult=students.pop()
-console.log(students)
-console.log(checkingTheResult)
-
-var splice=students.splice(0,2,"Reyan")
-console.log(students)
-console.log(splice)
-
-var diffrenciatingName= students.slice(0,1)
-console.log(students)
-console.log(diffrenciatingName)**/
-
-// WINDUP ARRAY CHAP
-
-//INDEX.OF
-
-/**var students = ["hassan", "moiz","rafay", "tayyab", "muneeb", "ameem"]
-
-var askWhichStudent = prompt("Which Student u want to know is present or not")
-if (students.indexOf(askWhichStudent) >=0){
-    console.log(`yes ${askWhichStudent} is available at seat.no ${students.indexOf(askWhichStudent)}`)
-    document.write(students[students.indexOf(askWhichStudent)])
+if(theDetailsFromTheDatabase.mobileNumber==detailsOfUser.mobileNumber)  {
+    if (theDetailsFromTheDatabase.password == detailsOfUser.password) {
+        console.log("Congratulations! You have Successfully Login. Taking You To Home Page")
+    }else {
+        console.log("Sorry! The password is incorrect.")
+    }
 }else {
-    console.log (`No! ${askWhichStudent} is not available in class`)
-
+    console.log("Oops! No User Found")
 }
 
-//INCLUDE
-
-var bakery = ["cakes", "pastery", "egg", "bread", "rusk", "cake rusk"]
-
-var askWhichProduct = prompt("Which poduct u want to buy")
-
-if(bakery.includes(askWhichProduct)){
-    console.log(`Yes! ${askWhichProduct} is available`)
-} else {
-    console.log(`No! ${askWhichProduct} is not available`)
-}**/
-
-// OBJECT
-
-var student1= {
-    name:"Hamza",
-    age:19,
-    address: "5-Star"
-}
-
-console.log(student1)
 
