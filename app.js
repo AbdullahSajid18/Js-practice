358167e8572ed442962606bba30fe6b6e2fdd31b
Mukhtar Ahmed Aziz <hafizmukhtar56@gmail.com>
2023-02-24 09:59:57 +0500

    Js practice

--- app.js
+++ app.js
@@ -1,30 +1,40 @@
-const theDetailsFromTheDatabase = {
-    firstName: "Syed",
-    lastName: "Hamza",
-    mobileNumber: "03190374204",
-    password: 626985,
-    dateOfBirth: 06,
-    monthOfBirth: 02,
-    yearOfBirth: 2004 ,
-    gender: "male"
+// const theDetailsFromTheDatabase = {
+//     firstName: "Syed",
+//     lastName: "Hamza",
+//     mobileNumber: "03190374204",
+//     password: 626985,
+//     dateOfBirth: 06,
+//     monthOfBirth: 02,
+//     yearOfBirth: 2004 ,
+//     gender: "male"
+// }
+
+// const mobileNumberOfUser = prompt("Type Your Mobile Number")
+// const passwordOfUser = prompt("Type Your password")
+
+// const detailsOfUser  = {
+//     mobileNumber : mobileNumberOfUser,
+//     password: passwordOfUser
+// }
+
+// if(theDetailsFromTheDatabase.mobileNumber==detailsOfUser.mobileNumber)  {
+//     if (theDetailsFromTheDatabase.password == detailsOfUser.password) {
+//         console.log("Congratulations! You have Successfully Login. Taking You To Home Page")
+//     }else {
+//         console.log("Sorry! The password is incorrect.")
+//     }
+// }else {
+//     console.log("Oops! No User Found")
+// }
+
+//FOR LOOP
+
+var myDailyRoutine= "Chai pio Biscuit khao:)"
+for (let a= 0; a< 3 ; a++) {
+    console.log(myDailyRoutine )
 }
 
-const mobileNumberOfUser = prompt("Type Your Mobile Number")
-const passwordOfUser = prompt("Type Your password")
-
-const detailsOfUser  = {
-    mobileNumber : mobileNumberOfUser,
-    password: passwordOfUser
+var hondaCars = ["City", "Civic", "Hrv", "Vezel", "Cd-125", "Crv", "Brv"]
+for (let a = 0; a < hondaCars.length; a++) {
+    console.log(`Honda ${hondaCars[a]} is available at floor ${a} `)
 }
-
-if(theDetailsFromTheDatabase.mobileNumber==detailsOfUser.mobileNumber)  {
-    if (theDetailsFromTheDatabase.password == detailsOfUser.password) {
-        console.log("Congratulations! You have Successfully Login. Taking You To Home Page")
-    }else {
-        console.log("Sorry! The password is incorrect.")
-    }
-}else {
-    console.log("Oops! No User Found")
-}
-
-
